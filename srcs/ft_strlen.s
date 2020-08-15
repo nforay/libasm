9524876c43c803de8f8e6e7faf78c8f80d486938
nforay <nforay@student.42.fr>
2020-08-15 21:58:39 +0200

    working ft_strlen

---
+++ srcs/ft_strlen.s
@@ -0,0 +1,25 @@
+; ------------------------------------------------------------------------------
+; size_t	ft_strlen(const char *str)
+; The ft_strlen()  function calculates the length of the string pointed to by
+; str, excluding the terminating null byte ('\0').
+; parameter register: rdi
+; returned value register: rax
+; ------------------------------------------------------------------------------
+
+			global	ft_strlen
+			section	.text
+
+ft_strlen:
+			xor		rax, rax			; i = 0
+			jmp		while				; enter while loop
+
+increment:
+			inc		rax					; i++
+
+while:
+			cmp		byte [rdi + rax], 0	; compare str[i] with 0
+			je		return				; if equal return
+			jmp		increment			; else increment
+
+return:
+			ret							; return (i)
